668778df99412b852116f5cc55a809bf3fac8fe7
Jose Julian Figueroa <josejulianfigueroa@gmail.com>
2019-01-10 10:15:17 -0300

    Seccion 12 Terminada Google Signin

--- config/config.js
+++ config/config.js
@@ -1,1 +1,5 @@
 module.exports.SEED = 'este-es-un-seed-dificil';
+
+// Google
+module.exports.GOOGLE_CLIENT_ID = '1000934228425-sau84me53rct4u84t4j9s6j2cs7eumu4.apps.googleusercontent.com';
+module.exports.GOOGLE_SECRET = '_YfoCn9nT7qZPM7l1FsREqyh';
